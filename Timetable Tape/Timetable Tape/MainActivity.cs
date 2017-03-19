b82cf6631862e9dcba806afd3d0faa1f2f1aa2ff
DESKTOP-8E53BPG\Robin-Laptop <robindenef@hotmail.com>
2017-03-19 12:03:37 +0300

    Created first UI.

--- Timetable Tape/Timetable Tape/MainActivity.cs
+++ Timetable Tape/Timetable Tape/MainActivity.cs
@@ -1,6 +1,12 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using InteractiveTimetable.BusinessLayer.Models;
+using InteractiveTimetable.BusinessLayer.Managers;
+using SQLite;
+using Android.Views;
+using Android.Content;
+using Android.Util;
 
 namespace Timetable_Tape
 {
@@ -11,9 +17,27 @@ namespace Timetable_Tape
         {
             base.OnCreate(bundle);
 
+            SetContentView(Resource.Layout.Main);
+
+            //Loading create timetable fragment
+            var trans = FragmentManager.BeginTransaction();
+            trans.Add(Resource.Id.FragmentContainer, new Fragment_Creating_Timetable_Tape(), "Fragment_Creating_Timetable_Tape");
+            trans.Commit();
+
+
+
+
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
+            Schedule schedule = new Schedule();
+
+            //string dbName = "Database.db3";
+            //SQLiteConnection connection = new SQLiteConnection(dbName);
+            //ScheduleManager sm = new ScheduleManager(connection);
+
         }
+
+        
     }
 }
 
